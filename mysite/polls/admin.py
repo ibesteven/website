2f65564188fdd79e5331fa2a372d3e129b66b558
steven <qiusiyuan24@gmail.com>
2021-11-20 19:24:42 -0500

    fixed admin error

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 from .models import Choice, Question
+
 class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -9,8 +13,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
+admin.site.register(Question, QuestionAdmin)
 list_display = ('question_text', 'pub_date', 'was_published_recently')
 list_filter = ['pub_date']
 search_fields = ['question_text']
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin 
 import datetime
 # Create your models here.
 class Question(models.Model):
